1f4293b01bdab71c67482b038fb385e3a98999af
cjr5716 <cjr5716@psu.edu>
2019-11-22 14:06:56 -0500

    Moved machines

--- src/main/java/edu/psu/bd/cs/cjr5717/jdbclab/main/jdbclab.java
+++ src/main/java/edu/psu/bd/cs/cjr5717/jdbclab/main/jdbclab.java
@@ -8,7 +8,9 @@ public class jdbclab {
 
         try {
 
-        Connection C = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\cjr5716\\IdeaProjects\\jdbclab\\src\\main\\resources\\mybookstore");
+//        Connection C = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\cjr5716\\IdeaProjects\\jdbclab\\src\\main\\resources\\mybookstore");
+        Connection C = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Owner\\IdeaProjects\\jdbc-lab\\src\\main\\resources\\mybookstore");
+            // Please use jdbc:sqlite:<path>
 
         System.out.print("Type the name of the book you are searching for: ");
 //        Scanner S = new Scanner(System.in);
@@ -16,7 +18,7 @@ public class jdbclab {
 
         String query2 = "Sushi, Anyone?";
 
-        String sql = "SELECT titles.title, authors.au_fname, authors.au_lname FROM authors INNER JOIN titles ON titles.au_id = authors.au_id WHERE titles.title LIKE ?";
+        String sql = "SELECT titles.title, authors.au_fname, authors.au_lname, pub_name FROM authors INNER JOIN titles ON titles.au_id = authors.au_id FROM publishers INNER JOIN WHERE titles.title LIKE ?";
 
         PreparedStatement P = C.prepareStatement(sql);
         P.setString(1, query2);
@@ -24,7 +26,9 @@ public class jdbclab {
         ResultSet R = P.executeQuery();
 
             while (R.next()) {
-                R.toString();
+                System.out.println(R.getString("title"));
+                System.out.println(R.getString("au_fname"));
+                System.out.println(R.getString("au_lname"));
             }
 
         C.close();
